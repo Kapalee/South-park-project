57a49614547456dbe443860d18c210c277f384a9
Kapalee <Shamvictor@hotmail.com>
2023-03-30 13:16:44 +0200

    sdf

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ async function initApp() {
   character.forEach(showCharacter);
 }
 
-async function getCharacter(url) {
-  const response = await fetch(url);
+async function getCharacter() {
+  const response = await fetch("data/southpark.json");
   const data = await response.json();
   return data;
 }
@@ -23,28 +23,23 @@ function showCharacter(person) {
     <p><strong>Aka: </strong><span>${person.nickname}</span></spa></p>
     </article>
       `;
-  document.querySelector("#characters").insertAdjacentHTML("beforeend");
+  document.querySelector("#characters").insertAdjacentHTML("beforeend", HTML);
   document.querySelector("#characters article:last-child").addEventListener("click", characterClicked);
 
   function characterClicked() {
+    document.querySelector("#dialogue-image").src = person.image;
     document.querySelector("#dialogue-name").textContent = person.name;
     document.querySelector("#dialogue-nickname").textContent = person.nickname;
     document.querySelector("#dialogue-occupation").textContent = person.occupation;
-    document.querySelector("#dialogue-voice").textContent = person.voicedBy;
+    document.querySelector("#dialogue-voicedby").textContent = person.voicedBy;
     document.querySelector("#dialogue-age").textContent = person.age;
     document.querySelector("#dialogue-gender").textContent = person.gender;
-    document.querySelector("#dialogue-eye").textContent = person.eyeColor;
-    document.querySelector("#dialogue-hair").textContent = person.hairColor;
-    document.querySelector("#dialogue-phrase").textContent = person.catchPhrase;
-    document.querySelector("#dialogue-appearance").textContent = person.firstAppearance;
     document.querySelector("#dialogue-religion").textContent = person.religion;
+    document.querySelector("#dialogue-hair").textContent = person.hairColor;
+    document.querySelector("#dialogue-schoolgrade").textContent = person.schoolGrade;
+    document.querySelector("#dialogue-episodes").textContent = person.episodes;
+    document.querySelector("#dialogue-appearances").textContent = person.appearances;
+    document.querySelector("#dialogue-firstAppearance").textContent = person.firstAppearance;
     document.querySelector("#dialogue").showModal();
   }
 }
-
-function showCharacterModal(list) {
-  showCharacter(person);
-}
-function showDialog(person) {}
-
-function closeDialog() {}
